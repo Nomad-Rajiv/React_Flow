27226d296970db0560514f9402eee2f02ce04a7c
Rajiv <rajivkum2021@gmail.com>
2024-10-09 19:05:33 +0530

    React_flow Assignment created

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { ReactFlow, Background, Controls, Panel, applyNodeChanges, applyEdgeChanges} from 'reactflow';
+import 'reactflow/dist/style.css';
+import './App.css'
+import { useCallback, useState } from 'react';
+
+
+
+function App() {
+  
+
+  return (
+    
+    <>
+    <h1>Hello world</h1>
+    </>
+  )
+}
+
+export default App
